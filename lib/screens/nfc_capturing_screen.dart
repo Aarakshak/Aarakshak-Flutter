84170db9c8fde0f6874abdb5095b9fd06357d250
NipunGandhi <nipungandhi21@gmail.com>
2023-07-30 14:49:21 +0530

    NFC Code

--- lib/screens/nfc_capturing_screen.dart
+++ lib/screens/nfc_capturing_screen.dart
@@ -1,9 +1,41 @@
 import 'package:aarakshak/ui_components/colors/color_code.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
-class NFCCapturingScreen extends StatelessWidget {
+class NFCCapturingScreen extends StatefulWidget {
   const NFCCapturingScreen({super.key});
+
+  @override
+  State<NFCCapturingScreen> createState() => _NFCCapturingScreenState();
+}
+
+class _NFCCapturingScreenState extends State<NFCCapturingScreen> {
+  String value = "Not connected";
+  String thisData = "No data";
+  String onemorevalue = "one more value";
+
+  Future<void> start() async {
+    bool isNfcAvailable = await NfcManager.instance.isAvailable();
+    if (isNfcAvailable) {
+      NfcManager.instance.startSession(
+        onDiscovered: (data) async {
+          if (data != null) {
+            setState(() {
+              value = "Connected";
+              thisData = data.data.toString();
+              onemorevalue = data.handle;
+            });
+          }
+        },
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +45,11 @@ class NFCCapturingScreen extends StatelessWidget {
         alignment: AlignmentDirectional.topEnd,
         children: [
           InkWell(
-            onTap: (){
+            onTap: () {
               Navigator.of(context).pop();
             },
             child: Container(
-              padding: const EdgeInsets.only(top: 40,right: 10),
+              padding: const EdgeInsets.only(top: 40, right: 10),
               child: SvgPicture.asset(
                 'assets/images/circular_close.svg',
               ),
@@ -56,6 +88,9 @@ class NFCCapturingScreen extends StatelessWidget {
                     color: AppColors.white,
                   ),
                 ),
+                Text(value),
+                Text(thisData),
+                Text(onemorevalue),
               ],
             ),
           ),
